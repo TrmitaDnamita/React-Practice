6f7ed3d0ab991400de4860547b09c77a98554c7b
TrmitaDnamita <franelasd@gmail.com>
2023-03-07 19:09:53 -0300

    Changes to be committed: 	modified:   src/App.js 	deleted:    src/App.test.js 	new file:   src/components/Greet.js 	modified:   src/index.js 	deleted:    src/logo.svg 	deleted:    src/reportWebVitals.js 	deleted:    src/setupTests.js

---
+++ src/components/Greet.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+/*
+
+function Greet() {
+    return <h1>Hello Friend</h1>
+}
+
+export default Greet
+
+*/
+//Simplifying the code above..
+//create and export constant Greet as a function that returns something
+export const Greet = () => <h1>9/11 was an inside job</h1>
