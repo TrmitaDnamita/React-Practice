37749886155ec55808c7ab5f457ddfff18f958c0
TrmitaDnamita <franelasd@gmail.com>
2025-07-01 19:07:16 -0300

    path_modified. nothing changed... on hold

--- projects/04-practice/src/components/PageFooter.jsx
+++ projects/04-practice/src/components/PageFooter.jsx
@@ -1,13 +1,32 @@
 export function PageFooter ({ actualPage, maxPage, changePageValue }) {
   const showBackButton = actualPage > 1 ? ' show' : ''
   const showNextButton = actualPage <= maxPage ? ' show' : ''
+
   return (
     <footer className='footer-class'>
       {showBackButton &&
         <button onClick={() => changePageValue(-1)}>
           {'< Back'}
         </button>}
-      <span>Page: {actualPage}/{maxPage}</span>
+      {Array.from({ length: 5 }, (_, i) => {
+        const page = actualPage - 2 + i
+        const isValid = page >= 1 && page <= maxPage
+        return (
+          <span
+            key={page}
+            onClick={(event) => changePageValue(parseInt(event.target.innerHTML, 10))}
+            className={`page-number${page === actualPage ? ' actual' : ''}`}
+            style={
+              {
+                fontWeight: page === actualPage ? 'bold' : 'normal',
+                opacity: page === actualPage ? 1 : 0.8
+              }
+            }
+          >
+            {isValid ? page : ''}
+          </span>
+        )
+      })}
       {showNextButton &&
         <button onClick={() => changePageValue(1)}>
           {'Next >'}
